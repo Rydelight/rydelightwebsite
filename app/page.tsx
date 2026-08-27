507474977f4d354bd542626ccbf9e742592bf43f
Rydelight <2.22338058e+08+Rydelight@users.noreply.github.com>
2026-08-27 06:27:00 +0000

    Improve homepage landmark structure

--- app/page.tsx
+++ app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
         <ServiceArea />
         <Testimonials />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
       <AIChatWidget />
     </>
   )
